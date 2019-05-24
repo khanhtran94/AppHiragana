b0fd2f88ff0ec734e4fd14e4216b26a64ccbc29b
khanhtt <khanhtrantung190894@gmail.com>
2019-05-25 04:31:02 +0700

    Initial Commit

---
+++ Japan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Japan
+//
+//  Created by stone on 5/25/19.
+//  Copyright © 2019 stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
